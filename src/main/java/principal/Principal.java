48813d9a1c4c92f6542161ec9d92588c9efe5a98
Nome do Usuário <email@gmail.com>
2025-07-08 22:15:58 -0300

    feat: criação do componente visual e configuração das funções de excluir e fechar.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package principal;
+
+import visao.FrmCadastroCliente;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmCadastroCliente frmCadastroCliente = new FrmCadastroCliente();
+        frmCadastroCliente.setVisible(true);
+    }
+}
